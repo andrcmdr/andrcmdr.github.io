1d8a3b41b816fa5f506dd50edaf853e8e2e60e98
Andy Francis <commandr@commandr.stream>
2019-03-22 07:13:25 +0600

    Update GLCI config

--- .gitlab-ci.sh
+++ .gitlab-ci.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #/usr/bin/env bash
 
+shopt -s globasciiranges
+shopt -s globstar
 shopt -s extglob
 shopt -s extquote
 shopt -s xpg_echo
@@ -8,5 +10,8 @@ shopt -s xpg_echo
 # set -f
 
 mkdir -vp ./public
-cp -vr -t ./public !(public|\.public|\.git|\.\.|\.)
+# cp -vr -t ./public !(public|\.public|\.git|\.\.|\.)
+# cp -vr -t ./public !(public|.public|.git|..|.)
+# cp -vr -t ./public *(.well-known|.gitlab*)!(public)
+cp -vr -t ./public *(.well-known|.gitlab*)!(public|.public|.git|..|.)
 
